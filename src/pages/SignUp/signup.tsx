7b1a141dd3a890c313a7eeb65acc83a3fb1c47de
AndreOS17 <andre.os908@gmail.com>
2021-10-23 19:46:33 -0300

    :zap: Atualizações e Criação da Página Novo Login

--- src/pages/SignUp/signup.tsx
+++ src/pages/SignUp/signup.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../../components/Footer/footer";
 import Header from "../../components/Header/header";
+
+import "./signup.css";
 
 function SignUp() {
     return (
         <div>
             <Header/>
-            <p>Hello World!</p>
-            <p>SignUp</p>
+            <div className="SignUpMain">
+                <h1 className="SignUpTitle">Senha de acesso</h1>
+                <h2 className="SignUpSubTitle">
+                    Para solicitar sua senha de acesso, preencha seu CPF:
+                </h2>
+
+                <form>
+                    <label htmlFor="User">CPF</label>
+                    <input id="User" type="text"/>
+
+                    <button type="submit">CONTINUAR</button>
+                </form>
+                
+            </div>
             <Footer/>
         </div>
     )
