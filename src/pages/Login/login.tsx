52abdb99597fc51649ccb48b49ed6477ab6c8b67
AndreOS17 <andre.os908@gmail.com>
2021-10-23 19:26:57 -0300

    :zap: Criação da Página Login

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,13 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../../components/Footer/footer";
 import Header from "../../components/Header/header";
+
+import "./login.css"
 
 function Login() {
     return (
         <div>
             <Header/>
-            <p>Hello World!</p>
-            <p>Login</p>
+            <div className="LoginMain">
+                <h1 className="LoginTitle">Já tem cadastro?</h1>
+                <h2 className="LoginSubTitle">Acesse sua conta</h2>
+
+                <form>
+                    <label htmlFor="User">CPF</label>
+                    <input id="User" type="text"/>
+
+                    <label htmlFor="Password">Senha</label>
+                    <input id="Password" type="password"/>
+
+                    <Link to="/Entrar/Novo-Login">Esqueci minha senha</Link>
+
+                    <button type="submit">CONTINUAR</button>
+                    <button className="CancelBT">
+                        <Link to="/Entrar/Novo-Login">PRIMEIRO ACESSO</Link>
+                    </button>
+                </form>
+
+            </div>
             <Footer/>
         </div>
     )
